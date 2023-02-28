e454f374a56d9f108b6a9ae772e748c4fbf5acb3
Gbeminiyi Ajayi <opefeni@yahoo.com>
2023-02-28 14:54:26 +0100

    update reactDom to version 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 // import react libraries
 import React from 'react';
-import ReactDOM   from 'react-dom';
+//import ReactDOM   from 'react-dom';
+import  ReactDOM  from 'react-dom/client';
 
 
 // import main App
 import App from './App';
 
-
 // render to dom
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+//ReactDOM.render(<App />, document.getElementById('root'));
 
